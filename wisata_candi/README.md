058fbf7ec8eeb7a45a8e01cf07c8f9ce5d603854
DanielWijayaa <ernestwijaya_2428250010@mhs.mdp.ac.id>
2025-11-07 19:36:18 +0700

    hoho

--- wisata_candi/README.md
+++
@@ -1,3 +0,0 @@
-# empty_flutter_2
-
-A new Flutter project.
--- wisata_candi/android/app/src/main/kotlin/com/example/empty_flutter_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.empty_flutter_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
